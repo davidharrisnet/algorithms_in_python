2295947f6a0e357271e933072fc6e9db0a04da89
Fazus Kazoo <fazuskazoo@gmail.com>
2022-05-26 20:57:46 -0400

    fibonacci and insertion sort

--- algorithms/sort/insertion.py
+++ algorithms/sort/insertion.py
@@ -3,6 +3,7 @@
 
 
 def sort(arr):
+    global swap_count
     for k in range(1, len(arr)): # start with the second item in the list
         key = arr[k]  # this is the key
 
@@ -10,13 +11,14 @@ def sort(arr):
         while l >= 0 and key < arr[l]:  # Then look at all the tokens to the left of key
             # The key is < arr[i] on the left,
             arr[l+1] = arr[l]  # Move arr[l] to the right
+            swap_count += 1
             l -= 1
         arr[l + 1] = key  #the item not < key receives key ( arr[l+1] has already been moved,
                                                              # so we can overwrite it. )
 
 if __name__ == "__main__":
     a = [7, 4, 6, 1, 5, 2, 9, 3, 0, 8]
-    #a = [2,1]
+    swap_count = 0
     print(a)
     sort(a)
     print(a)
