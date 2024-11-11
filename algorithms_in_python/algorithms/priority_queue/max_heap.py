d85bd94e3fb1562fae479b34712c863f460fc96b
David Harris <fazuskazoo@gmail.com>
2024-11-11 17:27:26 -0500

    Working on max heap

--- algorithms_in_python/algorithms/priority_queue/max_heap.py
+++ algorithms_in_python/algorithms/priority_queue/max_heap.py
@@ -18,14 +18,42 @@ class MaxHeap():
             self.arr[parent], self.arr[current] = self.arr[current], self.arr[parent]
             current = parent
             parent = current // 2
+    def pop(self):
+        if self.last_index == 0:
+            return None
+        # The greatest value to return
+        max_value = self.arr[1] 
         
+        # Swap out the root for the last element and shrink the heap
+        self.arr[1] = self.arr[self.last_index] 
+        self.arr[self.last_index] = None
+        self.last_index -= 1
+        
+        # Bubble the root down
+        
+        for i in range(1,self.last_index):
+            swap = 1
+            if 2*i <= self.last_index and (self.arr[swap] < self.arr[2*1]):
+                swap = 2*i+1
+            if i != swap:
+                self.arr[i], self.arr[swap] = self.arr[swap], self.arr[i]
+            else:
+                break
+        return max_value
+    def __repr__(self):
+        return "[ " + " ".join([f"{i}," for i in self.arr[1:self.last_index]]) + \
+                    f" {str(self.arr[self.last_index])} ]"
+        
+               
         
 if __name__ == "__main__":
     mh = MaxHeap(10)
-    arr = [None, 98,95,50,10,17,23]
-    mh.arr = arr
-    mh.last_index = len(arr) -1
-    mh.insert(85)
-    print(mh.arr)
+    arr = [98,95,50,10,17,23,85]
+    for i in arr:
+        mh.insert(i)    
+    print(mh)
+    print(mh.pop())
+    print(mh)
+    
        
     
